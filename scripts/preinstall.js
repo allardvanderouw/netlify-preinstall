c36f7b403b16fd34faeafae584ebd475b727822d
Allard van der Ouw <allard.van.der.ouw@oliver-it.nl>
2019-11-26 14:42:42 +0100

    no message

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,25 +1,25 @@
 /* eslint-disable */
 const fs = require('fs')
-const { exec } = require('child_process')
+const { spawnSync } = require('child_process')
 
 console.log('Running Preinstall script')
-console.log(process.env)
 
 try {
 	console.log('Read file')
 	fs.readFileSync('.npmrc', 'utf8')
 } catch (error) {
-	// File does not exist yet, create .npmrc and restart
-	console.log('Write file')
+	if (error && error.code === 'ENOENT') {
+		// File does not exist yet, create .npmrc and restart
+		console.log('Write file')
 
-	fs.writeFileSync('.npmrc', `//npm.pkg.github.com/:_authToken=${process.env.GITHUB_TOKEN}\n@oliverit:registry=https://npm.pkg.github.com/\n`)
-	fs.chmodSync('.npmrc', 0o600)
+		fs.writeFileSync('.npmrc', `//npm.pkg.github.com/:_authToken=${process.env.GITHUB_TOKEN}\n@oliverit:registry=https://npm.pkg.github.com/\n`)
+		fs.chmodSync('.npmrc', 0o600)
 
-	exec('yarn')
-	process.exit()
+		// Kill process
+		process.exitCode = 1
+		// exec('yarn')
+		spawnSync('yarn', { stdio: 'inherit' });
+		console.log('done?')
+		process.exitCode = 0
+	}
 }
-
-
-
-// const { exec } = require('child_process')
-// exec('yarn')
