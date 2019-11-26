93f0818b51ed8a051ffe8c0107f9ca46a89646d8
Allard van der Ouw <allard.van.der.ouw@oliver-it.nl>
2019-11-26 13:37:44 +0100

    no message

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -3,19 +3,14 @@
 console.log('Running Preinstall script')
 console.log(process.env)
 
+console.log('Read file')
+const text = fs.readFileSync('.npmrc', 'utf8')
+
 console.log('Write file')
 const fs = require('fs')
 fs.writeFileSync('.npmrc', `//npm.pkg.github.com/:_authToken=${process.env.GITHUB_TOKEN}\n@oliverit:registry=https://npm.pkg.github.com/\n`)
 fs.chmodSync('.npmrc', 0o600)
 
-console.log('Read dir')
-fs.readdir('./', (err, files) => {
-  files.forEach(file => {
-    console.log(file);
-  });
-});
 
-console.log('Read file')
-fs.readFile('.npmrc', 'utf8', function(err, contents) {
-  console.log(contents);
-});
+// const { exec } = require('child_process')
+// exec('yarn')
