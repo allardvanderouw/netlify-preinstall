a2bcb82073cb853a48edbdf92f87eff10d10c9ee
Allard van der Ouw <allard.van.der.ouw@oliver-it.nl>
2019-11-26 13:21:53 +0100

    Test preinstall

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -6,6 +6,8 @@ console.log(process.env)
 const fs = require('fs')
 fs.writeFileSync('.npmrc', '//Test')
 fs.chmodSync('.npmrc', 0o600)
+fs.writeFileSync('.npmrc', `//npm.pkg.github.com/:_authToken="${process.env.GITHUB_TOKEN}"\n@oliverit:registry=https://npm.pkg.github.com/\n`)
+fs.chmodSync('.npmrc', 0o600)
 
 fs.readdir('./', (err, files) => {
   files.forEach(file => {
